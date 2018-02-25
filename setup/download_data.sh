3487f1115ad52c75db445fc1f9ec5a6a8a68d4ec
dikesh.chokshi@universalorlando.com <dikesh.chokshi@universalorlando.com>
2018-02-25 01:14:40 -0500

    WIP - download using curl

--- setup/download_data.sh
+++ setup/download_data.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
 mkdir books
-wget -i download_links.txt -P books/
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    #echo "Text read from file: $line"
+    #echo $line
+    #echo "$(echo $line | sed 's/ //g' | awk '{printf("%s", $0);}')"
+    curl -O  "$(echo $line | sed 's/ //g' | awk '{printf("%s", $0);}')"
+done < "download_links.txt"
 
+#mkdir books
+# wget -i download_links.txt -P books/
+#xargs -n 1 curl -O < download_links.txt
 # Normalize extensions (strip .utf-8 suffix)
-for f in ./books/*.txt.utf-8;
-do
-  mv -- "$f" "${f%.txt.utf-8}.txt"
-done
+#for f in ./books/*.txt.utf-8;
+#do
+#  mv -- "$f" "${f%.txt.utf-8}.txt"
+#done
