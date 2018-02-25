4d806dcb6824d848f04f1462b06eef0522e5a166
dikesh.chokshi@universalorlando.com <dikesh.chokshi@universalorlando.com>
2018-02-25 03:06:59 -0500

    Downloading URLs correctly.

--- setup/download_data.sh
+++ setup/download_data.sh
@@ -3,18 +3,12 @@ if [[ ! -d "books" ]]; then
     mkdir "books"
 fi
 while IFS='' read -r line || [[ -n "$line" ]]; do
-echo $line
-
-A=${line##*/}
-echo  "$(echo $line  | awk '{printf("%s", $0)}')"
-#curl -o ./books/$A "$(echo $line  | awk '{printf("%s", $0);}')"
+  A=${line##*/}
+  curl -o ./books/$A "$(echo $line  | awk '{printf("%s", $0)}')"
 done < "download_links.txt"
 
-#mkdir books
-# wget -i download_links.txt -P books/
-#xargs -n 1 curl -O < download_links.txt
-# Normalize extensions (strip .utf-8 suffix)
-#for f in ./books/*.txt.utf-8;
-#do
-#  mv -- "$f" "${f%.txt.utf-8}.txt"
-#done
+# Remove trailing underscore
+for f in ./books/*.*;
+do
+  mv -- "$f" "${f%_}"
+done
